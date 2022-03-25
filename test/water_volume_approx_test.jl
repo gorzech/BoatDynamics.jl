c1bda68f96b0d6cf14501644149ac60ff57340a5
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2022-03-25 14:02:28 +0200

    Add extra tests for x1vh2o and z1vh2o. Seems work.

--- test/water_volume_approx_test.jl
+++ test/water_volume_approx_test.jl
@@ -34,3 +34,77 @@ end
     @test_broken bd.vh2om(the, bh2o) ≈ 2V_m_b rtol = 1e-6
     @test bd.vh2orn(the, bh2o) ≈ 2V_r_b rtol = 1e-3
 end
+
+@testset "Check if approximation of z1vh2o is correct" begin
+    tol = eps()
+    @test abs(bd.z1vh2o(0.0, bd.zmax - tol)) < tol
+    
+    z1v_max = bd.z1vh2o(0.0, 0.0)
+    z1v_m = 2integrate_middle(u -> u[2] * fi2m(u))
+    z1v_f = 2integrate_front(u -> u[2] * fi2f(u))
+    z1v_r = 2integrate_rear(u -> u[2] * fi2r(u))
+    @test z1v_max ≈ z1v_m + z1v_f + z1v_r rtol = 1e-5
+    # compute for some bh2o value
+    bh2o = 0.111245
+    z1v_m = 2integrate_middle(u -> u[2] * fi2m(u); zmin = bh2o)
+    z1v_f = 2integrate_front(u -> u[2] * fi2f(u); zmin = bh2o)
+    z1v_r = 2integrate_rear(u -> u[2] * fi2r(u); zmin = bh2o)
+    @test bd.z1vh2o(0.0, bh2o) ≈ z1v_m + z1v_f + z1v_r rtol = 1e-6
+
+    bh2o = 0.05
+    the = deg2rad(1)
+    z_limit_W(f_limit) = u -> (u[2] < water_line_z(u[1], bh2o, the) || f_limit(u))
+
+    z1v_m = 2integrate_middle(u -> u[2] * fi2m(u); flimit = z_limit_W(flimitm))
+    z1v_f = 2integrate_front(u -> u[2] * fi2f(u); flimit = z_limit_W(flimitf))
+    z1v_r = 2integrate_rear(u -> u[2] * fi2r(u); flimit = z_limit_W(flimitr))
+
+    z1vh2o = z1v_m + z1v_f + z1v_r
+    @test bd.z1vh2o(the, bh2o) ≈ z1vh2o rtol = 5e-4
+
+    bh2o = 0.07
+    the = -deg2rad(1)
+    z1v_m = 2integrate_middle(u -> u[2] * fi2m(u); flimit = z_limit_W(flimitm))
+    z1v_f = 2integrate_front(u -> u[2] * fi2f(u); flimit = z_limit_W(flimitf))
+    z1v_r = 2integrate_rear(u -> u[2] * fi2r(u); flimit = z_limit_W(flimitr))
+
+    z1vh2o = z1v_m + z1v_f + z1v_r
+    @test bd.z1vh2o(the, bh2o) ≈ z1vh2o rtol = 5e-4
+end
+
+@testset "Check if approximation of x1vh2o is correct" begin
+    tol = eps()
+    @test abs(bd.x1vh2o(0.0, bd.zmax - tol)) < tol
+    
+    x1v_max = bd.x1vh2o(0.0, 0.0)
+    x1v_m = 2integrate_middle(u -> u[1] * fi2m(u))
+    x1v_f = 2integrate_front(u -> u[1] * fi2f(u))
+    x1v_r = 2integrate_rear(u -> u[1] * fi2r(u))
+    @test x1v_max ≈ x1v_m + x1v_f + x1v_r rtol = 1e-5
+    # compute for some bh2o value
+    bh2o = 0.111245
+    x1v_m = 2integrate_middle(u -> u[1] * fi2m(u); zmin = bh2o)
+    x1v_f = 2integrate_front(u -> u[1] * fi2f(u); zmin = bh2o)
+    x1v_r = 2integrate_rear(u -> u[1] * fi2r(u); zmin = bh2o)
+    @test bd.x1vh2o(0.0, bh2o) ≈ x1v_m + x1v_f + x1v_r rtol = 1e-6
+
+    bh2o = 0.05
+    the = deg2rad(1)
+    z_limit_W(f_limit) = u -> (u[2] < water_line_z(u[1], bh2o, the) || f_limit(u))
+
+    x1v_m = 2integrate_middle(u -> u[1] * fi2m(u); flimit = z_limit_W(flimitm))
+    x1v_f = 2integrate_front(u -> u[1] * fi2f(u); flimit = z_limit_W(flimitf))
+    x1v_r = 2integrate_rear(u -> u[1] * fi2r(u); flimit = z_limit_W(flimitr))
+
+    x1vh2o = x1v_m + x1v_f + x1v_r
+    @test bd.x1vh2o(the, bh2o) ≈ x1vh2o rtol = 1e-2
+
+    bh2o = 0.07
+    the = -deg2rad(1)
+    x1v_m = 2integrate_middle(u -> u[1] * fi2m(u); flimit = z_limit_W(flimitm))
+    x1v_f = 2integrate_front(u -> u[1] * fi2f(u); flimit = z_limit_W(flimitf))
+    x1v_r = 2integrate_rear(u -> u[1] * fi2r(u); flimit = z_limit_W(flimitr))
+
+    x1vh2o = x1v_m + x1v_f + x1v_r
+    @test bd.x1vh2o(the, bh2o) ≈ x1vh2o rtol = 5e-4
+end
